b0bb3de5e6e930ef431195f365c320786b657c4e
Tom Craig <thetomcraig@icloud.com>
2020-01-29 20:17:09 -0800

    removing vimwiki for now

---
+++ vim_utils.sh
@@ -0,0 +1,10 @@
+" Function to show the vim-highlight group associated with the word under the
+" cursor
+nmap <leader>sp :call <SID>SynStack()<CR>
+function! <SID>SynStack()
+  if !exists("*synstack")
+    return
+  endif
+  echo map(synstack(line('.'), col('.')), 'synIDattr(v:val, "name")')
+endfunc
+
